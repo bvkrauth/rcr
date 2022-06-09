97256404f454da1b6ece6ddeedee0727685c7d5f
Brian Krauth <bkrauth@sfu.ca>
2022-06-08 17:49:22 -0700

    Rename pytest fixtures to avoid conflicts
    Signed-off-by: Brian Krauth <bkrauth@sfu.ca>

--- python/testing/conftest.py
+++ python/testing/conftest.py
@@ -13,22 +13,22 @@ sys.path.append("../")
 from rcr import RCR, read_data  # pylint: disable=wrong-import-position
 
 
-@pytest.fixture
-def moment_vector():
+@pytest.fixture(name="moment_vector")
+def fixture_moment_vector():
     """get moment vector from test data"""
     mv = read_data("testing/testin1.txt")[3]
     return mv
 
 
-@pytest.fixture
-def dat():
+@pytest.fixture(name="dat")
+def fixture_dat():
     """get test data from web page"""
     fname = "http://www.sfu.ca/~bkrauth/code/rcr_example.dta"
     return pd.read_stata(fname)
 
 
-@pytest.fixture
-def rcr_formula():
+@pytest.fixture(name="rcr_formula")
+def fixture_rcr_formula():
     """construct formula for test example"""
     rcr_left = "SAT + Small_Class ~ "
     rcr_right1 = "White_Asian + Girl + Free_Lunch + White_Teacher + "
@@ -36,42 +36,42 @@ def rcr_formula():
     return rcr_left + rcr_right1 + rcr_right2
 
 
-@pytest.fixture
-def endog(dat, rcr_formula):
+@pytest.fixture(name="endog")
+def fixture_endog(dat, rcr_formula):
     """get endogenous variables"""
     endog = patsy.dmatrices(rcr_formula, dat)[0]
     return endog
 
 
-@pytest.fixture
-def exog(dat, rcr_formula):
+@pytest.fixture(name="exog")
+def fixture_exog(dat, rcr_formula):
     """get endogenous variables"""
     exog = patsy.dmatrices(rcr_formula, dat)[1]
     return exog
 
 
-@pytest.fixture
-def model(endog, exog):
+@pytest.fixture(name="model")
+def fixture_model(endog, exog):
     """construct RCR mdodel"""
     return RCR(endog, exog)
 
 
-@pytest.fixture
-def results(model):
+@pytest.fixture(name="results")
+def fixture_results(model):
     """fit RCR mdodel"""
     return model.fit()
 
 
-@pytest.fixture
-def weights(dat):
+@pytest.fixture(name="weights")
+def fixture_weights(dat):
     """get weights"""
     wt = np.mod(dat["TCHID"], 2)
     wt.name = "TCHID_wt"
     return wt
 
 
-@pytest.fixture
-def clusters(dat):
+@pytest.fixture(name="clusters")
+def fixture_clusters(dat):
     """get cluster IDs"""
     clust = dat["TCHID"]
     return clust
